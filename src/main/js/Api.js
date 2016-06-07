6c60a2b806a6fffbde79b1ec5ab58798212c96ee
root <root@localhost>
2016-06-07 11:07:24 +1000

    Masking

--- src/main/js/Api.js
+++ src/main/js/Api.js
@@ -71,12 +71,25 @@ Api.prototype.logOutputBody = function(apim, bodi, loggingTerminal) {
 
 	try {
 		var transactionid = apim.getvariable('message.headers.x-global-transaction-id');
-		var bodyPayload = {"x-global-transaction-id":transactionid,  body:bodi, loggingTerminal:loggingTerminal};
-		this.logger.debug(JSON.stringify(bodyPayload));
+		var bodyPayload = {"x-global-transaction-id":transactionid, loggingTerminal:loggingTerminal, body:bodi};
+		var str = JSON.stringify(bodyPayload);
+		var str = this.mask(str);
+		this.logger.debug(str);
 	} catch(e) {
 		this.logger.error(e);
 		throw e;
 	}
+}
+
+Api.prototype.mask = function(str) {
+	var maskedStr = str;
+	
+	// Mask credit card account number
+	maskedStr = maskedStr.replace(/\b(\d{12})(\d{4})\b/ig, 'xxxxxxxxxxxx$2');	
+	// Mask reference number
+	maskedStr = maskedStr.replace(/(?:"(refNum|referenceNumber)"\s*:\s*")\b(\d{4})\d+(\d{4})\b/ig, '"$1": $2xxxxxxxx$3');
+	
+	return maskedStr;
 }
 
 
