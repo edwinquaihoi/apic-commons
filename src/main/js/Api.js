7b22684ce9492635b4da551c477020cda77f6aec
root <root@localhost>
2016-06-07 16:47:17 +1000

    Fix a missing quote

--- src/main/js/Api.js
+++ src/main/js/Api.js
@@ -91,7 +91,7 @@ Api.prototype.mask = function(str) {
 	// Mask credit card account number
 	maskedStr = maskedStr.replace(/\b(\d{12})(\d{4})\b/ig, 'xxxxxxxxxxxx$2');	
 	// Mask reference number
-	maskedStr = maskedStr.replace(/(?:"(refNum|referenceNumber)"\s*:\s*")\b(\d{4})\d+(\d{4})\b/ig, '"$1": $2xxxxxxxx$3');
+	maskedStr = maskedStr.replace(/(?:"(refNum|referenceNumber)"\s*:\s*")\b(\d{4})\d+(\d{4})\b/ig, '"$1": "$2xxxxxxxx$3');
 	
 	return maskedStr;
 }
